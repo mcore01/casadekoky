44d7ea2d0aa093401731d21d3ef9f7c475b16492
mcore01 <rommel.melendez@gmail.com>
2026-01-30 15:38:49 -0600

    Update content.js

--- api/content.js
+++ api/content.js
@@ -1,6 +1,11 @@
-export default function handler(req, res) {
-  res.json({
-    test: "Backend funcionando",
-    hero: "Casa de Koky ya es plataforma"
-  });
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.SUPA_URL,
+  process.env.SUPA_KEY
+);
+
+export default async function handler(req, res) {
+  const { data } = await supabase.from("content").select("*");
+  res.json(data);
 }
