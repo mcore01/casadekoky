428de23c402d6a0a41444cd5fa811529a23aee67
mcore01 <rommel.melendez@gmail.com>
2026-03-04 23:34:00 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // 1. CONFIGURACIÓN (RELLENA CON TUS DATOS DE SETTINGS > API)
-/* --> const SUPABASE_URL = 'https://pmejptyabrcqyfonzjax.supabase.co';
+const SUPABASE_URL = 'https://pmejptyabrcqyfonzjax.supabase.co';
 const SUPABASE_KEY = 'sb_publishable_HzS1qYKDxvSPdjveYAkD2Q_ew6WEgDS';
-const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY); */ 
+const _supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY); 
 
 // 2. ESTADO GLOBAL
 let currentLang = 'es';
@@ -107,5 +107,6 @@ document.addEventListener('keydown', (e) => {
         if (e.key === "ArrowLeft") changeImage(-1);
     }
 });
+
 
 
